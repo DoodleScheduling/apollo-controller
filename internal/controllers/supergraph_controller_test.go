c3b70be2c4f7eb35433b26211d31f9ad5ad28534
Raffael Sahli <raffael.sahli@doodle.com>
2025-12-09 14:49:23 +0100

    fix: new tests

---
+++ internal/controllers/supergraph_controller_test.go
@@ -0,0 +1,306 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/DoodleScheduling/apollo-controller/api/v1beta1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("SuperGraph controller", func() {
+	const (
+		timeout  = time.Second * 4
+		interval = time.Millisecond * 200
+	)
+
+	var eventuallyMatchExactConditions = func(ctx context.Context, instanceLookupKey types.NamespacedName, reconciledInstance *v1beta1.SuperGraph, expectedStatus *v1beta1.SuperGraphStatus) {
+		Eventually(func() error {
+			err := k8sClient.Get(ctx, instanceLookupKey, reconciledInstance)
+			if err != nil {
+				return err
+			}
+
+			return needsExactConditions(expectedStatus.Conditions, reconciledInstance.Status.Conditions)
+		}, timeout, interval).Should(BeNil())
+	}
+
+	When("reconciling a suspended SuperGraph", func() {
+		supergraphName := fmt.Sprintf("supergraph-%s", randStringRunes(5))
+
+		It("should not update the status", func() {
+			By("creating a new SuperGraph")
+			ctx := context.Background()
+
+			gi := &v1beta1.SuperGraph{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      supergraphName,
+					Namespace: "default",
+				},
+				Spec: v1beta1.SuperGraphSpec{
+					Suspend: true,
+				},
+			}
+			Expect(k8sClient.Create(ctx, gi)).Should(Succeed())
+
+			By("waiting for the reconciliation")
+			instanceLookupKey := types.NamespacedName{Name: supergraphName, Namespace: "default"}
+			reconciledInstance := &v1beta1.SuperGraph{}
+
+			eventuallyMatchExactConditions(ctx, instanceLookupKey, reconciledInstance, &v1beta1.SuperGraphStatus{})
+		})
+	})
+
+	When("it reconciles a supergraph without a schema", func() {
+		supergraphName := fmt.Sprintf("supergraph-%s", randStringRunes(5))
+		var supergraph *v1beta1.SuperGraph
+
+		It("creates a new supergraph", func() {
+			ctx := context.Background()
+
+			supergraph = &v1beta1.SuperGraph{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      supergraphName,
+					Namespace: "default",
+				},
+				Spec: v1beta1.SuperGraphSpec{},
+			}
+			Expect(k8sClient.Create(ctx, supergraph)).Should(Succeed())
+		})
+
+		It("should update the supergraph status", func() {
+			ctx := context.Background()
+			instanceLookupKey := types.NamespacedName{Name: supergraphName, Namespace: "default"}
+			reconciledInstance := &v1beta1.SuperGraph{}
+
+			expectedStatus := &v1beta1.SuperGraphStatus{
+				ObservedGeneration: 1,
+				Conditions: []metav1.Condition{
+					{
+						Type:    v1beta1.ConditionReady,
+						Status:  metav1.ConditionFalse,
+						Reason:  "ReconciliationFailed",
+						Message: "schema not found",
+					},
+				},
+			}
+			eventuallyMatchExactConditions(ctx, instanceLookupKey, reconciledInstance, expectedStatus)
+		})
+
+		It("should not create a service", func() {
+			instanceLookupKey := types.NamespacedName{Name: fmt.Sprintf("apollo-router-%s", supergraphName), Namespace: "default"}
+			reconciledInstance := &corev1.Service{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, instanceLookupKey, reconciledInstance)
+			}, timeout, interval).ShouldNot(BeNil())
+		})
+
+		It("cleans up", func() {
+			ctx := context.Background()
+			Expect(k8sClient.Delete(ctx, supergraph)).Should(Succeed())
+		})
+	})
+
+	/*When("it reconciles a supergraph with a schema", func() {
+		schemaName := fmt.Sprintf("supergraph-%s", randStringRunes(5))
+		supergraphName := fmt.Sprintf("supergraph-%s", randStringRunes(5))
+		var schema *v1beta1.SuperGraphSchema
+		var supergraph *v1beta1.SuperGraph
+
+		It("creates a new schema", func() {
+			ctx := context.Background()
+
+			schema = &v1beta1.SuperGraphSchema{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      schemaName,
+					Namespace: "default",
+				},
+				Spec: v1beta1.SuperGraphSchemaSpec{},
+			}
+			Expect(k8sClient.Create(ctx, schema)).Should(Succeed())
+		})
+
+		It("creates a new supergraph", func() {
+			ctx := context.Background()
+
+			supergraph = &v1beta1.SuperGraph{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      supergraphName,
+					Namespace: "default",
+				},
+				Spec: v1beta1.SuperGraphSpec{
+					Schema: corev1.LocalObjectReference{
+						Name: schemaName,
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, supergraph)).Should(Succeed())
+		})
+
+		It("should update the supergraph status", func() {
+			ctx := context.Background()
+			instanceLookupKey := types.NamespacedName{Name: supergraphName, Namespace: "default"}
+			reconciledInstance := &v1beta1.SuperGraph{}
+
+			expectedStatus := &v1beta1.SuperGraphStatus{
+				ObservedGeneration: 1,
+				Conditions: []metav1.Condition{
+					{
+						Type:    v1beta1.ConditionReady,
+						Status:  metav1.ConditionTrue,
+						Reason:  "ReconciliationSuccessful",
+						Message: fmt.Sprintf("deployment/apollo-router-%s created", supergraphName),
+					},
+				},
+			}
+			eventuallyMatchExactConditions(ctx, instanceLookupKey, reconciledInstance, expectedStatus)
+		})
+
+		It("should create a service", func() {
+			instanceLookupKey := types.NamespacedName{Name: fmt.Sprintf("apollo-router-%s", supergraphName), Namespace: "default"}
+			reconciledInstance := &corev1.Service{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, instanceLookupKey, reconciledInstance)
+			}, timeout, interval).Should(BeNil())
+
+			Expect(reconciledInstance.Spec.Selector).To(Equal(map[string]string{
+				"app.kubernetes.io/instance":   "apollo-router",
+				"app.kubernetes.io/name":       "apollo-router",
+				"apollo-controller/supergraph": supergraphName,
+			}))
+			Expect(reconciledInstance.OwnerReferences[0].Name).Should(Equal(supergraph))
+		})
+
+		It("should create a deployment", func() {
+			instanceLookupKey := types.NamespacedName{Name: fmt.Sprintf("apollo-router-%s", supergraphName), Namespace: "default"}
+			reconciledInstance := &appsv1.Deployment{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, instanceLookupKey, reconciledInstance)
+			}, timeout, interval).Should(BeNil())
+
+			Expect(reconciledInstance.Spec.Template.ObjectMeta.Labels).To(Equal(map[string]string{
+				"app.kubernetes.io/instance":               "apollo-router",
+				"app.kubernetes.io/name":                   "apollo-router",
+				"swagger-supergraph-controller/supergraph": supergraphName,
+			}))
+
+			Expect(reconciledInstance.ObjectMeta.Labels).To(Equal(map[string]string{
+				"app.kubernetes.io/instance":   "apollo-router",
+				"app.kubernetes.io/name":       "apollo-router",
+				"apollo-controller/supergraph": supergraphName,
+			}))
+
+			Expect(reconciledInstance.Spec.Template.Spec.Containers[0].Name).To(Equal("router"))
+			Expect(reconciledInstance.Spec.Template.Spec.Containers[0].Image).To(Equal("ghcr.io/apollo/router:v2.9.0"))
+			Expect(reconciledInstance.OwnerReferences[0].Name).Should(Equal(supergraph))
+		})
+
+		It("cleans up", func() {
+			ctx := context.Background()
+			Expect(k8sClient.Delete(ctx, supergraph)).Should(Succeed())
+		})
+	})
+
+	When("it reconciles a supergraph with a custom template", func() {
+		supergraphName := fmt.Sprintf("supergraph-%s", randStringRunes(5))
+		var supergraph *v1beta1.SuperGraph
+		var replicas int32 = 3
+
+		It("creates a new supergraph", func() {
+			ctx := context.Background()
+
+			supergraph = &v1beta1.SuperGraph{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      supergraphName,
+					Namespace: "default",
+				},
+				Spec: v1beta1.SuperGraphSpec{
+					DeploymentTemplate: &v1beta1.DeploymentTemplate{
+						Spec: v1beta1.DeploymentSpec{
+							Replicas: &replicas,
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name: "apollo-router",
+											Env: []corev1.EnvVar{
+												{
+													Name:  "FOO",
+													Value: "bar",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, supergraph)).Should(Succeed())
+		})
+
+		It("should update the supergraph status", func() {
+			ctx := context.Background()
+			instanceLookupKey := types.NamespacedName{Name: supergraphName, Namespace: "default"}
+			reconciledInstance := &v1beta1.SuperGraph{}
+
+			expectedStatus := &v1beta1.SuperGraphStatus{
+				ObservedGeneration: 1,
+				Conditions: []metav1.Condition{
+					{
+						Type:    v1beta1.ConditionReady,
+						Status:  metav1.ConditionTrue,
+						Reason:  "ReconciliationSuccessful",
+						Message: fmt.Sprintf("deployment/apollo-router-%s created", supergraphName),
+					},
+				},
+			}
+			eventuallyMatchExactConditions(ctx, instanceLookupKey, reconciledInstance, expectedStatus)
+		})
+
+		It("should create a deployment", func() {
+			instanceLookupKey := types.NamespacedName{Name: fmt.Sprintf("apollo-router-%s", supergraphName), Namespace: "default"}
+			reconciledInstance := &appsv1.Deployment{}
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, instanceLookupKey, reconciledInstance)
+			}, timeout, interval).Should(BeNil())
+
+			Expect(reconciledInstance.Spec.Template.ObjectMeta.Labels).To(Equal(map[string]string{
+				"app.kubernetes.io/instance":   "apollo-router",
+				"app.kubernetes.io/name":       "apollo-router",
+				"apollo-controller/supergraph": supergraphName,
+			}))
+
+			Expect(reconciledInstance.ObjectMeta.Labels).To(Equal(map[string]string{
+				"app.kubernetes.io/instance":   "apollo-router",
+				"app.kubernetes.io/name":       "apollo-router",
+				"apollo-controller/supergraph": supergraphName,
+			}))
+
+			Expect(reconciledInstance.Spec.Template.Spec.Containers[0].Name).To(Equal("router"))
+			Expect(reconciledInstance.Spec.Template.Spec.Containers[0].Image).To(Equal("ghcr.io/apollo/router:v2.9.0"))
+			Expect(reconciledInstance.Spec.Template.Spec.Containers[0].Env).To(Equal([]corev1.EnvVar{
+				{
+					Name:  "FOO",
+					Value: "bar",
+				},
+			}))
+			Expect(reconciledInstance.OwnerReferences[0].Name).Should(Equal(supergraph))
+		})
+
+		It("cleans up", func() {
+			ctx := context.Background()
+			Expect(k8sClient.Delete(ctx, supergraph)).Should(Succeed())
+		})
+	})*/
+})
